d524db3f2ce0573b80297530fe6af6463fad420a
lucaspereirasouza <lucaspereiradesouza2163@outlook.com>
2025-01-07 01:24:08 -0300

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+      },
       keyframes: {
         marquee: {
-          "0%": { transform: "translateX(100%)" },
-          "100%": { transform: "translateX(-100%)" },
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-50%)' },
         },
-      },
-      animation: {
-        marquee: "marquee 10s linear infinite",
+       
       },
     },
   },
